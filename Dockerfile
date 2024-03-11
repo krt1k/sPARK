29da73ce461ef80b6d625e29be34ef228a8a137e
Kishorekarthik P <kishorekarthik.p@rently.com>
2024-03-11 16:31:50 +0530

    Update Flask app with new templates and Dockerfile configuration

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.10.13-slim-bullseye
 COPY . /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-CMD ["python", "./web/app.py"]
+WORKDIR /app/web
+RUN apt-get update && apt-get install -y sqlite3
+RUN pip install -r /app/requirements.txt
+EXPOSE 5000
+CMD ["python", "app.py"]
--- web/app.py
+++ web/app.py
@@ -1,33 +1,131 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy 
-from flask_bcrypt import Bcrypt
+import bcrypt
 
 
 app = Flask(__name__)
 
 # db
-bcrypt = Bcrypt(app)
+# bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'ToPsecret'
 db = SQLAlchemy(app)
-# db.init_app(app)
-# class User:
+app.config['SECRET_KEY'] = 'ToPsecret'
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        # self.is_admin = is_admin
 
-@app.route('/')
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/create_admin', methods=['GET', 'POST'])
+def create_admin():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['pwd']
+        
+        admin = User(username, email, password)
+        admin.is_admin = True
+        db.session.add(admin)
+        db.session.commit()
+
+        return "<script>alert('Admin created!');</script>"
+    return render_template('create_admin.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if session['is_admin']:
+        user = User.query.filter_by(username=session['username']).first()
+        return render_template('admin.html', user=user, email=session['email'])
+    return render_template('login.html', error='You are not a admin!')
+
+
+@app.route('/reset')
+def reset():
+    db.drop_all()
+    db.create_all()
+    return 'Database reset'
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    # return "<h1>Hello World!</h1><script>alert('Hello World!')</script>"
+    if request.method == 'POST':
+        pass
     return render_template('index.html')
 
-@app.route('/login')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('email', None)
+    session.pop('is_admin', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['pwd']
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.check_password(password):
+                session['username'] = user.username
+                session['email'] = user.email
+                session['is_admin'] = user.is_admin
+
+                if user.is_admin == True:
+                    return redirect(url_for('admin'))
+
+                return redirect(url_for('dashboard'))
+            else:
+                # notify user of invalid password
+                return render_template('login.html', error='Invalid username/password')
+        else:
+            return redirect(url_for('register') )
+    
     return render_template('login.html')
 
-@app.route('/register')
-def register():
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['pwd']
+        
+        user = User(username, email, password)
+        db.session.add(user)
+        db.session.commit()
+
+        print(username, email, password)
+
+        return redirect(url_for('login'))
+    
     return render_template('register.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if session['username']:
+        user = User.query.filter_by(username=session['username']).first()
+        return render_template('dashboard.html', user=user, email=session['email']) 
+    return render_template('login.html', error='You are not logged in')
 
 if __name__ == '__main__':
     app.run(debug=True)
